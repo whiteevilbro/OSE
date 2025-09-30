8c56202d2fc92067722b2cf8fd5f315b5478ad78
whiteevilbro <maks-yakovlev21@yandex.ru>
2025-09-30 11:47:44 +0700

    IF clear

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@
 # Build tools
 NASM = nasm -f bin 
 SHELL := /bin/bash
-VERSION = string
+VERSION = video
 
 # =============================================================================
 # Tasks
@@ -32,17 +32,21 @@ debug: build
 	qemu-system-i386 -cpu pentium2 -m 1g -fda boot.img -monitor stdio -device VGA -s -S &
 	gdb
 
-BYTES = 29
-OFFSET = $$((0x7C00 + $(BYTES)))
-SKIP = $$((510 - $(BYTES)))
-DATA = 16
+# BYTES = 32
+# OFFSET = $$((0x7C00 + $(BYTES)))
+# SKIP = $$((510 - $(BYTES)))
+DATA_START = 0x7C03
+DATA = 20
 
 d: dis
 dis: disasm
 disasm: build
 	$(NASM) src/helloworld_$(VERSION).asm -o ./boot.bin
 	$(eval HLT := 0x$(shell (ndisasm -b 16 -o 0x7c00 -a -p intel ./boot.bin) | (grep -m 1 -P -o '[0-9A-F]{8}(?=\s+F4\s+hlt)' /dev/stdin)))
-	ndisasm -b 16 -o 0x7c00 -k $(OFFSET),$(SKIP) -k 0x7C02,$(DATA) -s 0x7C12 -a -p intel ./boot.bin
-	echo Estimated bytes: $$(($(HLT) + 1 - 0x7C00 - $(DATA)))
+	$(eval BYTES := $$$$(( $(HLT) - 0x7C00 + 1)))
+	$(eval OFFSET := $$$$((0x7C00 + $$(BYTES))))
+	$(eval SKIP := $$$$((510 - $$(BYTES))))
+	ndisasm -b 16 -o 0x7c00 -k $(OFFSET),$(SKIP) -k $(DATA_START),$(DATA) -s 0x7C00 -a -p intel ./boot.bin
+	@echo Estimated bytes: $$(($(BYTES) - $(DATA)))
 
 .PHONY: all build clean test debug d dis disasm
--- src/helloworld_video.asm
+++ src/helloworld_video.asm
@@ -1,28 +1,35 @@
 [BITS 16]
 [ORG 0x7C00]
 
-; === 1 === (14 bytes)
+; cs:ip is set to 0x7C00, right?
+; save ip for later
+call st ; near call (push ip to stack) - 3 bytes
 
-jmp st ; 2 bytes
+; 0x7C03  ; 20 bytes of data
+dw 0xDEAD ; di - available
+dw 0xDEAD ; si - available
+dw 0xDEAD ; bp - available
+dw 0xDEAD ; <> - discarded
+dw 0x0000 ; bx -> buffer offset
+dw 0x0000 ; dx -> dh=0 dl=0 -> zeroth head, zeroth (A) floppy 
+dw 0x0002 ; cx -> ch=0 cl=2 -> second sector, zeroth cylinder
+dw 0x0201 ; ax -> ah=2 al=1 -> read one sector
+dw 0xB800 ; es -> buffer base
+dw 0x0002 ; flags -> all flags cleared. 1 in reserved bit
 
-; 0x13
-; si bp <ax> bx dx cx ax 
-db 0x00, 0xb8, 0x00, 0x00, 0x00, 0x7c, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x01, 0x02
+st: ; 0x7c00 + 0x0017; 0x17 == 20 + 3
+push cs   ; byte
+; load ss:sp with 0x7C00 + size(call rel16)
+pop ss    ; byte
+pop sp    ; byte 
+; right now ss:sp is set to 0x7C03, funny, innit?
+popa      ; byte
+pop es    ; byte
+popf      ; byte ; can be replaced with cli, but this is more stylish
+int 0x13  ; 2 bytes
+hlt       ; byte
 
-; es
-db 0x00, 0xb8
-
-st: ; 0x7c00 + 0x0012
-push BYTE 0
-pop ss
-mov sp, 0x7C00
-; push 0x7C
-; push DWORD 0x0
-
-popa
-pop es
-int 0x13
-hlt
+; Total: 3 + (1 + 1 + 1) + (1 + 1 + 1) + (2 + 1) = 12
 
 times 510-($-$$) db 0
 dw 0xAA55

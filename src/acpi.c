b39b5a5ad6393e19e380d7e20c00a67a8e7c73bb
Yakovlev Maksim <maks-yakovlev21@yandex.ru>
2025-11-19 23:47:58 +0700

    acpi

--- src/acpi.c
+++ src/acpi.c
@@ -12,16 +12,18 @@
 #define BIOS_ROM_END 0x0FFFFF
 
 const RSDP_t* RSDP = NULL;
+const RSDT_t* RSDT = NULL;
+const FADT_t* FADT = NULL;
 
-bool validate_RSDP(const RSDP_t* ptr) {
+static bool validate_RSDP(const RSDP_t* ptr) {
   if (memcmp(ptr->signature, "RSD PTR ", 8)) {
     return false;
   }
   uint8_t checksum = 0;
   for (size_t i = 0; i < sizeof(RSDP_t); i++) {
-    checksum += *(((const uint8_t*) ptr) + i);
+    checksum += ((const uint8_t*) ptr)[i];
   }
-  return checksum == 0;
+  return !checksum;
 }
 
 static inline const RSDP_t* find_RSDP(const void* EBDA_start) {
@@ -46,7 +48,29 @@ static inline const RSDP_t* find_RSDP(const void* EBDA_start) {
   return NULL;
 }
 
+static bool validate_checksum(const DescriptionTableHeader* table) {
+  uint8_t checksum = 0;
+  for (size_t i = 0; i < table->length; i++) {
+    checksum += ((const uint8_t*) table)[i];
+  }
+  return !checksum;
+}
+
 void init_acpi(const void* EBDA_start) {
   // find valid RSDP
   RSDP = find_RSDP(EBDA_start);
+
+  if (validate_checksum(&RSDP->RSDT->header))
+    RSDT = RSDP->RSDT;
+
+  if (!RSDT)
+    return;
+
+  for (size_t i = 0; i < (RSDT->header.length - sizeof(DescriptionTableHeader)) / sizeof(DescriptionTableHeader*); i++) {
+    if (!FADT && !memcmp(RSDT->entry[i]->signature, "FACP", 4)) {
+      if (validate_checksum(RSDT->entry[i])) {
+        FADT = (const FADT_t*) RSDT->entry[i];
+      }
+    }
+  }
 }
--- src/acpi.h
+++ src/acpi.h
@@ -6,11 +6,29 @@
 #pragma pack(push, 1)
 
 typedef struct {
+  char signature[4];
+  uint32_t length;
+  uint8_t revision;
+  uint8_t checksum;
+  uint8_t oemid[6];
+  uint8_t oem_table_id[8];
+  uint32_t oem_revision;
+  uint32_t creator_id;
+  uint32_t creator_revision;
+} DescriptionTableHeader;
+
+typedef struct {
+  // signature: RSDT
+  DescriptionTableHeader header;
+  const DescriptionTableHeader* entry[]; // size according to the header `length` field
+} RSDT_t;
+
+typedef struct {
   char signature[8];
   uint8_t checksum;
   uint8_t oemid[6];
   uint8_t revision;
-  void* RSDT;
+  RSDT_t* RSDT;
 } RSDP_t;
 
 typedef struct {
@@ -18,7 +36,7 @@ typedef struct {
   uint8_t checksum;
   uint8_t oemid[6];
   uint8_t revision;
-  void* RSDT_address;
+  RSDT_t* RSDT;
 
   uint32_t length;
   void* XSDT;
@@ -26,7 +44,75 @@ typedef struct {
   uint8_t reserved[3];
 } XSDP_t;
 
+typedef struct {
+  // signature: FACP
+  DescriptionTableHeader header;
+
+  uint32_t firmware_ctrl;
+  uint32_t dsdt;
+
+  uint8_t int_model;
+  uint8_t preferred_pm_profile;
+
+  uint16_t sci_int;
+  uint32_t smi_cmd;
+
+  uint8_t acpi_enable;
+  uint8_t acpi_disable;
+
+  uint8_t s4bios_req;
+  uint8_t pstate_cnt;
+
+  uint32_t pm1a_evt_blk;
+  uint32_t pm1b_evt_blk;
+
+  uint32_t pm1a_cnt_blk;
+  uint32_t pm1b_cnt_blk;
+
+  uint32_t pm2_cnt_blk;
+
+  uint32_t pm_tmr_blk;
+
+  uint32_t gpe0_blk;
+  uint32_t gpe1_blk;
+
+  uint8_t pm1_evt_len;
+  uint8_t pm1_cnt_len;
+  uint8_t pm2_cnt_len;
+
+  uint8_t pm_tm_len;
+
+  uint8_t gpe0_blk_len;
+  uint8_t gpe1_blk_len;
+
+  uint8_t gpe1_base;
+
+  uint8_t cst_cnt;
+
+  uint16_t p_lvl2_lat;
+  uint16_t p_lvl3_lat;
+
+  uint16_t flush_size;
+  uint16_t flush_stride;
+
+  uint8_t duty_offset;
+  uint8_t duty_width;
+
+  uint8_t day_alrm;
+  uint8_t mon_alrm;
+  uint8_t century;
+
+  uint16_t iapc_boot_arch;
+  uint8_t reserved;
+
+  uint32_t flags;
+} FADT_t;
+
 #pragma pack(pop)
+
+extern const RSDP_t* RSDP;
+extern const RSDT_t* RSDT;
+extern const FADT_t* FADT;
 
 void init_acpi(const void* EBDA_start);
 
